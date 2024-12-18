6e18cca75bef0a39f9c606c9769ca3ae9ff62a57
ViceSergo <sergeitrufanov73@mail.ru>
2024-12-18 21:23:34 +0300

    demoqa_Home

---
+++ tests/test_check_swag.py
@@ -0,0 +1,18 @@
+from pages.swag_labs import SwagLabs
+
+
+def test_check_icon(browser):
+    swag_labs_page= SwagLabs(browser)
+    swag_labs_page.visit()
+    assert swag_labs_page.exist_icon()
+    assert swag_labs_page.exist_password()
+
+def test_check_login(browser):
+    swag_labs_page= SwagLabs(browser)
+    swag_labs_page.visit()
+    assert swag_labs_page.exist_login()
+
+def test_check_password(browser):
+    swag_labs_page= SwagLabs(browser)
+    swag_labs_page.visit()
+    assert swag_labs_page.exist_password()
