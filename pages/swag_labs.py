b71f157ec2e69ed0ae8303c82efd2691a8c8709a
ViceSergo <sergeitrufanov73@mail.ru>
2024-12-18 21:33:46 +0300

    demoqa_Home

--- pages/swag_labs.py
+++ pages/swag_labs.py
@@ -3,23 +3,30 @@ from selenium.common.exceptions import NoSuchElementException
 
 class SwagLabs(BasePage):
 
-    def exist_icon(self):
-        try:
-            self.find_element(locator='div.login_logo')
-        except NoSuchElementException:
-            return False
-        return True
+    # def exist_icon(self):
+    #     try:
+    #         self.find_element(locator='div.login_logo')
+    #     except NoSuchElementException:
+    #         return False
+    #     return True
+    #
+    # def exist_login(self):
+    #     try:
+    #         self.find_element(locator='#user-name')
+    #     except NoSuchElementException:
+    #         return False
+    #     return True
+    #
+    # def exist_password(self):
+    #     try:
+    #         self.find_element(locator='#password')
+    #     except NoSuchElementException:
+    #         return False
+    #     return True
 
-    def exist_login(self):
+    def exist_element(self,locator):
         try:
-            self.find_element(locator='#user-name')
-        except NoSuchElementException:
-            return False
-        return True
-
-    def exist_password(self):
-        try:
-            self.find_element(locator='#password')
+            self.find_element(locator=locator)
         except NoSuchElementException:
             return False
         return True
--- tests/test_check_swag.py
+++ tests/test_check_swag.py
@@ -4,15 +4,17 @@ from pages.swag_labs import SwagLabs
 def test_check_icon(browser):
     swag_labs_page= SwagLabs(browser)
     swag_labs_page.visit()
-    assert swag_labs_page.exist_icon()
-    assert swag_labs_page.exist_password()
+    assert swag_labs_page.exist_element('div.login_logo')
+    # assert swag_labs_page.exist_icon()
 
 def test_check_login(browser):
     swag_labs_page= SwagLabs(browser)
     swag_labs_page.visit()
-    assert swag_labs_page.exist_login()
+    assert swag_labs_page.exist_element('#user-name')
+    # assert swag_labs_page.exist_login()
 
 def test_check_password(browser):
     swag_labs_page= SwagLabs(browser)
     swag_labs_page.visit()
-    assert swag_labs_page.exist_password()
+    assert swag_labs_page.exist_element('#password')
+    # assert swag_labs_page.exist_password()
